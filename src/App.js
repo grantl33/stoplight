f854adf8b58aa3e349965399c59f70977c883426
Grant Lee <grantlee@Grants-MacBook-Pro.local>
2023-10-26 09:02:46 -0700

    stoplight code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+// Stoplight has the following durations:
+// Red light: 4000ms
+// Yellow light: 500ms
+// Green light: 3000ms
+const config = {
+  red: {
+    duration: 4000,
+    next: 'yellow'
+  },
+  yellow: {
+    duration: 500,
+    next: 'green'
+  },
+  green: {
+    duration: 3000,
+    next: 'red'
+  }
+}
 
 function App() {
+  const [color, setColor] = useState('red');
+  let stoplightClasses = ['stoplight'];
+  stoplightClasses.push(color);
+
+  useEffect(() => {
+    const { next, duration } = config[color];
+    const timeout = setTimeout(() => setColor(next), duration);
+
+    return () => {
+      // cleanup
+      clearTimeout(timeout);
+    }
+  }, [color])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="">
+        <h1>Stoplight Demo</h1>
       </header>
+      <main>
+        <div className={stoplightClasses.join(' ')}>
+          <div className='light'></div>
+          <div className='light'></div>
+          <div className='light'></div>
+        </div>
+      </main>
     </div>
   );
 }
